1ff6c323adca5f04d49adb1a885769ffbff0759a
celchita <celchita07@gmail.com>
2024-02-19 13:42:01 +0100

    decimo commit

--- name-of-your-project/src/main.jsx
+++ name-of-your-project/src/main.jsx
@@ -14,6 +14,7 @@ import Root, {
   loader as rootLoader,
   action as rootAction,
 } from "./routes/root";
+import EditContact from "./routes/edit";
 
 const router = createBrowserRouter([
   {
@@ -26,6 +27,11 @@ const router = createBrowserRouter([
       {
         path: "contacts/:contactId",
         element: <Contact />,
+        loader: contactLoader,
+      },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact />,
         loader: contactLoader,
       },
     ],
